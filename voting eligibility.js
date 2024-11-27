4207ff36d923eac4bae7144433d88734590b7a50
D-Ajith <doddi.ajith2003@gmail.com>
2024-11-27 20:22:34 +0530

    Js operators and if else

---
+++ voting eligibility.js
@@ -0,0 +1,51 @@
+// Voting Eligibility
+var age=prompt("Enter Your Age")
+if(age>=18){
+    console.log("Eligible to vote")
+}
+else{
+    console.log("Not Eligible to vote")
+}
+
+// Marriage Eligibility
+var Male=prompt("Enter Male age")
+if(Male>=21){
+    console.log("Male Eligible to marriage")
+}
+else{
+    console.log("Male Not Eligible to marriage")
+}
+var Female=prompt("Enter Female age")
+if(Female>=18){
+    console.log("Female Eligible to marriage")
+}
+else{
+    console.log("Female Not Eligible to marriage")
+}
+
+// Driving License Eligibility
+var License=prompt("Enter Your Age")
+if(License>=18){
+    console.log("Eligible For lincense")
+}
+else{
+    console.log("Not Eligible For lincense")
+}
+
+// Exam Eligibility
+var examatt=prompt("Enter attendence")
+if(examatt>=75){
+     console.log("Eligible for exam ")
+}
+else{
+    console.log("Not Eligible for exam pay condonation ")
+}
+
+// Senior Citizen Discount Eligibility
+var discount=prompt("Enter your age")
+if(discount>=60){
+    console.log("Eligible for Senior Citizen Discount ")
+}
+else{
+    console.log("Not Eligible for Senior Citizen Discount ")
+}
